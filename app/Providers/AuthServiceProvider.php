42b4433ab00aed02f446f4ba5dbd191ff7f0f1f5
MAIDUONGTHAO <mai.duong.thao@asterisk.com>
2021-07-08 15:55:18 +0700

    policy+repository

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -13,7 +13,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        // 'App\Model' => 'App\Policies\ModelPolicy',
+        'App\Models\Task\Task' => 'App\Policies\TaskPolicy',
     ];
 
     /**
@@ -25,6 +25,5 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
     }
 }
