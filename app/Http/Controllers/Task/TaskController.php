798a6e2f07e470a5619512f4e98fe332cd0cf981
MAIDUONGTHAO <mai.duong.thao@asterisk.com>
2021-07-08 17:38:41 +0700

    done-quickstart

---
+++ app/Http/Controllers/Task/TaskController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers\Task;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\TaskRequest;
+use Illuminate\Http\Request;
+use App\Models\Task;
+use App\Repositories\TaskRepository;
+
+class TaskController extends Controller
+{
+    protected $task;
+    public function __construct(TaskRepository $tasks)
+    {
+        $this->tasks = $tasks;
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        return View('tasks.index', [
+            'tasks' => $this->tasks->forUser($request->user()),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(TaskRequest $request)
+    {
+        $request->user()->tasks()->create([
+            'name' => $request->name,
+        ]);
+
+        return redirect('/tasks');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request)
+    {
+        $task = Task::find($request->id);
+        if (isset($task)) {
+            $this->authorize('destroy', $task);
+            $task->delete();
+        }
+        return redirect('/tasks');
+    }
+}
